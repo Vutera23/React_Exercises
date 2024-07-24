eb1aa3c3b236ae4199c01cfa07e24795f4196699
Emanuele <emanuelevutera@yahoo.it>
2024-07-24 17:03:37 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { HelloWorld } from "./HelloWorld";
+
+export function App() {
+  return (
+    <div>
+      <h1>My Awesome Application</h1>
+      <hr />
+      <HelloWorld />
+    </div>
+  );
+}
