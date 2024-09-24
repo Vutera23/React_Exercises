4f974610a5a21f8c456e9607b157e27011838f1a
emanuele_vutera <emanuelevutera@yahoo.it>
2024-09-24 16:49:23 +0200

    conditional rendering

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -7,7 +7,14 @@ export function Welcome({ name = Paolo, age = 18 }) {
         Welcome,
         <strong> {name}</strong>!
       </p>
-      <Age age={age} />
+      {age > 18 ? <Age age={age} /> : <p>You are very young!</p>}
+      {Boolean(age) ? <Age age={age} /> : ""}
+      {age > 18 && age < 65 ? <Age age={age} /> : <p>You are very young!</p>}
+      {age > 18 && name === "John" ? (
+        <Age age={age} />
+      ) : (
+        <p>You are very young!</p>
+      )}
     </div>
   );
 }
