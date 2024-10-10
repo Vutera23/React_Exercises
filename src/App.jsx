1c7d377de61f40c6c97aea942a5047a4c8eb3049
emanuele_vutera <emanuelevutera@yahoo.it>
2024-10-10 17:32:36 +0200

    list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { FocusableInput } from "./FocusableInput";
+import { Color } from "./Color";
+import { Colors } from "./Colors";
 
 export function App() {
   return (
     <div>
       <h1>My Awesome Application</h1>
       <hr />
-      <FocusableInput />
+      <h3>Single li</h3>
+      <Color color={{ id: 1, name: "red" }} />
+      <h3>Ul list</h3>
+      <Colors
+        colors={[
+          { id: 1, name: "red" },
+          { id: 2, name: "green" },
+          { id: 3, name: "blue" },
+          { id: 4, name: "amber" },
+        ]}
+      />
     </div>
   );
 }
---
+++ src/Colors.jsx
@@ -0,0 +1,9 @@
+export function Colors({ colors }) {
+  return (
+    <ul>
+      {colors.map((color) => (
+        <li key={color.id}>{color.name}</li>
+      ))}
+    </ul>
+  );
+}
